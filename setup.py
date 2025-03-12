2e6c3966f0e2018f46d411d6cdf12d7cb7ce9a6c
dioctech <admin@dioc.tech>
2025-03-12 14:30:28 +0300

    Update setup

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='bert-extractive-summarizer',
+setup(name='gpt-extractive-summarizer',
       version='0.10.1',
-      description='Extractive Text Summarization with BERT',
-      keywords=['bert', 'pytorch', 'machine learning',
+      description='Extractive Text Summarization with OpenAI',
+      keywords=['openai', 'pytorch', 'machine learning',
                 'deep learning', 'extractive summarization', 'summary'],
       long_description=open("README.md", "r", encoding='utf-8').read(),
       long_description_content_type="text/markdown",
-      url='https://github.com/dmmiller612/bert-extractive-summarizer',
-      download_url='https://github.com/dmmiller612/bert-extractive-summarizer/archive/0.10.1.tar.gz',
-      author='Derek Miller',
-      author_email='dmmiller612@gmail.com',
-      install_requires=['transformers', 'scikit-learn', 'spacy'],
+      url='https://github.com/madeinmo/gpt-extractive-summarizer',
+      download_url='https://github.com/madeinmo/gpt-extractive-summarizer/archive/0.10.1.tar.gz',
+      author='Timur Zhilyaev',
+      author_email='timurgan.gi2@gmail.com',
+      install_requires=['openai', 'scikit-learn', 'spacy'],
       license='MIT',
       packages=find_packages(),
-      zip_safe=False)
+      zip_safe=False
+)
